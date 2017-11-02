e45a31753240b40cb177bd82f8482c35af9fad91
Margaret Berry and Sara Spink <margaret@>
2017-11-02 16:37:22 -0700

    edit project details shows on admin route but no buttons exist yet

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service'
 
 @Component({
   selector: 'app-edit-project',
   templateUrl: './edit-project.component.html',
-  styleUrls: ['./edit-project.component.css']
+  styleUrls: ['./edit-project.component.css'],
+  providers: [ProjectService]
 })
 export class EditProjectComponent implements OnInit {
-
-  constructor() { }
+  @Input() selectedProject;
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
   }
